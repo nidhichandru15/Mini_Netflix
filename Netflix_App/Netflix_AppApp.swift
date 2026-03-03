df22ac8e89ace25f4dcedc8ee732003f5b3d1d29
Nidhi Chandru <nidhi.chandru@zopping.com>
2026-03-03 15:40:27 +0530

    Initial Commit

---
+++ Netflix_App/Netflix_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_AppApp.swift
+//  Netflix_App
+//
+//  Created by Zopping on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
