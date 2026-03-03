a2d829838d18eaadf476ddfc304a7fc84007ef15
HarshaZopping <harsha.r@zopping.com>
2026-03-03 17:09:49 +0530

    added foregroundStyle in home screen

--- Netflix_App/Resources/HomeView.swift
+++ Netflix_App/Resources/HomeView.swift
@@ -36,6 +36,7 @@ struct HomeView: View {
                 
             
                 TextField("Search movies...", text: $viewModel.searchText)
+					.foregroundStyle(.white)
                     .padding()
                     .background(Color.gray.opacity(0.2))
                     .cornerRadius(10)
